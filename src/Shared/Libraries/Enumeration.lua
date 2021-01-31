f3123a2bd451fa49773b0194a8a4a9eea21dfb14
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-01-31 03:08:02 -0600

    Create Enumeration.lua

---
+++ src/Shared/Libraries/Enumeration.lua
@@ -0,0 +1,49 @@
+local Deus = shared.Deus()
+
+local Output = Deus:Load("Deus.Output")
+local TableUtils = Deus:Load("Deus.TableUtils")
+
+local function __tostring(self)
+    return ("Deus.Enumeration.%s.%s"):format(self.EnumType, self.Name)
+end
+
+local Enumeration = {}
+
+Enumeration.__index = function(self, i)
+    local v = rawget(Enumeration, i)
+    if type(v) == "table" then
+        return TableUtils.deepCopy(v)
+    else
+        return v
+    end
+end
+
+function Enumeration.addEnumItem(enumName, enumItemName, value)
+    local enum = Enumeration[enumName]
+    local proxy = newproxy(true)
+    local meta = getmetatable(proxy)
+
+    meta.__metatable = "[Enumeration] Locked metatable"
+    meta.__tostring = __tostring
+
+    meta.Name = enumItemName
+    meta.Value = value
+    meta.EnumType = enumName
+
+    enum[enumItemName] = proxy
+
+    return proxy
+end
+
+function Enumeration.addEnum(enumName, list)
+    Output.assert(not Enumeration[enumName], "Name for enum '%s' is already in use", enumName)
+    Enumeration[enumName] = {}
+
+    for enumItemName, value in pairs(list) do
+        Enumeration.addEnumItem(enumName, enumItemName, value)
+    end
+
+    return TableUtils.deepCopy(Enumeration[enumName])
+end
+
+return setmetatable(Enumeration, Enumeration)
