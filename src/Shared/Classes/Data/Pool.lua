954a2db0326e6a6fa33ad4cae20a72e22661bac5
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-02-20 03:21:53 -0600

    Added pool

---
+++ src/Shared/Classes/Data/Pool.lua
@@ -0,0 +1,52 @@
+local Pool = {}
+
+Pool.ClassName = "Deus.Pool"
+
+Pool.Extendable = true
+
+Pool.Replicable = true
+
+Pool.Methods = {}
+
+Pool.Events = {}
+
+-- Gets an item from the pool, if one is not available runs the newItemFunc function provided in constructor
+function Pool.Methods:Get()
+    local item = self.Items[1]
+
+    if item then
+        table.remove(self.Items, 1)
+        return item
+    else
+        return self.NewItemFunc()
+    end
+end
+
+-- This should be run when done using the item to return it back to the pool
+function Pool.Methods:Add(item)
+    table.insert(self.Items, item)
+end
+
+function Pool:Constructor(newItemFunc)
+    self.NewItemFunc = newItemFunc
+end
+
+-- Destroys everything in the pool
+function Pool:Deconstructor()
+    return self.Items
+end
+
+function Pool:start()
+    self.PrivateProperties = {
+        NewItemFunc = self:Load("Deus.Symbol").new("None"),
+        Items = {}
+    }
+
+    self.PublicReadOnlyProperties = {}
+
+    self.PublicReadAndWriteProperties = {}
+
+    return self:Load("Deus.BaseObject").new(self)
+end
+
+return Pool
