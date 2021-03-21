d5c922a60ce1d7f4a58679dc150e322b7235de31
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-03-21 13:16:04 -0500

    Updated Output to work with scripts that have no script environment variable

--- src/Shared/Libraries/Debug/Output.lua
+++ src/Shared/Libraries/Debug/Output.lua
@@ -1,24 +1,32 @@
+local function getScriptName()
+    local env = getfenv(3)
+    if env.script and typeof(env.script) == "Instance" then
+        return env.script.Name
+    end
+    return ""
+end
+
 local Output = {}
 
 function Output.print(msg, args)
     if type(args) ~= "table" then
         args = {args}
     end
-    print(("[Deus][%s] "):format(getfenv(2).script.Name).. (msg or ""):format(unpack(args or {})))
+    print(("[Deus][%s] "):format(getScriptName()).. (msg or ""):format(unpack(args or {})))
 end
 
 function Output.warn(msg, args)
     if type(args) ~= "table" then
         args = {args}
     end
-    warn(("[Deus][%s] "):format(getfenv(2).script.Name).. (msg or ""):format(unpack(args or {})))
+    warn(("[Deus][%s] "):format(getScriptName()).. (msg or ""):format(unpack(args or {})))
 end
 
 function Output.error(msg, args, level)
     if type(args) ~= "table" then
         args = {args}
     end
-    error(("[Deus][%s] "):format(getfenv(2).script.Name).. (msg or ""):format(unpack(args or {})), 2 + (level or 0))
+    error(("[Deus][%s] "):format(getScriptName()).. (msg or ""):format(unpack(args or {})), 2 + (level or 0))
 end
 
 function Output.assert(condition, msg, args, level)
@@ -26,7 +34,7 @@ function Output.assert(condition, msg, args, level)
         args = {args}
     end
     if not condition then
-        error(("[Deus][%s] "):format(getfenv(2).script.Name).. (msg or ""):format(unpack(args or {})), 2 + (level or 0))
+        error(("[Deus][%s] "):format(getScriptName()).. (msg or ""):format(unpack(args or {})), 2 + (level or 0))
     end
 end
 
